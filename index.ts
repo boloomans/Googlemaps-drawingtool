38bf84339e3b17e627b512f9e643ad5d62c13867
Bo Loomans <bo.loomans@ivengi.com>
2023-03-30 15:16:51 +0200

    feat(map): add textarea to print drawn path to

--- index.ts
+++ index.ts
@@ -7,38 +7,39 @@
 // This example requires the Drawing library. Include the libraries=drawing
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=drawing">
-var map;
-var overlay;
+let map;
+let overlay;
+let polygonsArr = [];
 
 function initMap(): void {
-  google.maps.Polygon.prototype.getGeoJSON = function () {
-    let geoJSON = {
-      type: 'Polygon',
-      coordinates: [],
-    };
-
-    let paths = this.getPaths().getArray();
-
-    for (path of paths) {
-      let pathArray = [];
-      let points = path.getArray();
-      let firstPoint = false;
-
-      for (point of points) {
-        if (firstPoint === false) {
-          firstPoint = point;
-        }
-
-        pathArray.push([point.lng(), point.lat()]);
-      }
-
-      pathArray.push([firstPoint.lng(), firstPoint.lat()]);
-
-      geoJSON.coordinates.push(pathArray);
-    }
-
-    return geoJSON;
-  };
+  // google.maps.Polygon.prototype.getGeoJSON = function () {
+  //   let geoJSON = {
+  //     type: 'Polygon',
+  //     coordinates: [],
+  //   };
+  //
+  //   let paths = this.getPaths().getArray();
+  //
+  //   for (path of paths) {
+  //     let pathArray = [];
+  //     let points = path.getArray();
+  //     let firstPoint = false;
+  //
+  //     for (point of points) {
+  //       if (firstPoint === false) {
+  //         firstPoint = point;
+  //       }
+  //
+  //       pathArray.push([point.lng(), point.lat()]);
+  //     }
+  //
+  //     pathArray.push([firstPoint.lng(), firstPoint.lat()]);
+  //
+  //     geoJSON.coordinates.push(pathArray);
+  //   }
+  //
+  //   return geoJSON;
+  // };
 
   map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
     center: { lat: -34.397, lng: 150.644 },
@@ -46,7 +47,7 @@ function initMap(): void {
   });
 
   const drawingManager = new google.maps.drawing.DrawingManager({
-    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingMode: google.maps.drawing.OverlayType.POLYGON,
     drawingControl: true,
     drawingControlOptions: {
       position: google.maps.ControlPosition.TOP_CENTER,
@@ -78,7 +79,15 @@ function initMap(): void {
     'overlaycomplete',
     function (event) {
       var paths = event.overlay.getPath().getArray();
-      console.log(paths.toString());
+      // @ts-ignore
+      polygonsArr.push(`[${paths.toString().replaceAll('(', '[').replaceAll(')', ']')}]`)
+
+      // Path for iManager, setting zones
+      // @ts-ignore
+      $('#vertices').val(`[${paths.toString().replaceAll('(', '[').replaceAll(')', ']')}]`);
+
+      // All polygons drawn on map in Array[].
+      // $('#vertices').val(`[${polygonsArr.toString().replaceAll('(', '[').replaceAll(')', ']')}]`);
       if (event.type == 'circle') {
         var radius = event.overlay.getRadius();
       }
